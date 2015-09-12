e373c79ef23d419e2b179ad163ef1f04bc1ca4b4
sumeet-shah-90 <sumeet-shah@outlook.com>
2015-09-12 12:49:22 +0530

    Initial Commit with Windows Phone App Solution

---
+++ Project1.App/Project1.App/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Project1.App.Resources;
+
+namespace Project1.App
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
