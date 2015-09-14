95b76d76a4ba88286b63173f26140c18fb66947c
sumeet-shah-90 <sumeet-shah@outlook.com>
2015-09-15 01:53:05 +0530

    Missing Rijndael and UserManager Files.

---
+++ Project1.API/Project1.API/Utility/UserManager.cs
@@ -0,0 +1,52 @@
+﻿using Project1.Common;
+using Project1.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace Project1.API.Utility
+{
+    public static class UserManager
+    {
+        public static string GenerateSecurityHash(ApplicationUser userToSave, string password)
+        {
+            string initVector = password;
+
+            int remainingDigits = 16 - password.Length;
+
+            if (remainingDigits > 0)
+            {
+                for (int i = 1; i <= remainingDigits; i++)
+                {
+                    initVector += "*";
+                }
+            }
+
+            return RJindael.Encrypt(userToSave.JoinTS.ToBinary().ToString(), userToSave.UserID.ToString(), userToSave.PhoneNumber, "SHA1", 29, initVector, 256);
+        }
+
+        public static string GeneratePassword(ApplicationUser userToSave, string password)
+        {
+            string initVector = password;
+
+            int remainingDigits = 16 - password.Length;
+
+            if (remainingDigits > 0)
+            {
+                for (int i = 1; i <= remainingDigits; i++)
+                {
+                    initVector += "*";
+                }
+            }
+
+            return RJindael.Encrypt(userToSave.SecurityHash, userToSave.UserID.ToString(), userToSave.PhoneNumber, "SHA1", 37, initVector, 256);
+        }
+
+        public static string GenerateAccessToken(ApplicationUser userToLogin)
+        {
+            return RJindael.Encrypt(Json.Encode(userToLogin), userToLogin.Password, userToLogin.SecurityHash, "SHA1", 47, "****************", 256);
+        }
+    }
+}
---
+++ Project1.API/Project1.Common/RJindael.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Common
+{
+    public static class RJindael
+    {
+        /// <summary>
+        /// Encrypt data using Rijndael encryption.
+        /// </summary>
+        /// <param name=plainText></param>
+        /// <param name=passPhrase></param>
+        /// <param name=saltValue></param>
+        /// <param name=hashAlgorithm></param>
+        /// <param name=passwordIterations></param>
+        /// <param name=initVector></param>
+        /// <param name=keySize></param>
+        /// <returns></returns>
+        public static string Encrypt(string plainText, string passPhrase, string saltValue, string hashAlgorithm, int passwordIterations, string initVector, int keySize)
+        {
+            string cipherText = string.Empty;
+
+            try
+            {
+                byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+                byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+                byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+                PasswordDeriveBytes password = new PasswordDeriveBytes(
+
+                passPhrase, saltValueBytes, hashAlgorithm, passwordIterations);
+
+                byte[] keyBytes = password.GetBytes(keySize / 8);
+
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+
+                symmetricKey.Mode = CipherMode.CBC;
+
+                ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
+
+                MemoryStream memoryStream = new MemoryStream();
+
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+
+                cryptoStream.FlushFinalBlock();
+
+                byte[] cipherTextBytes = memoryStream.ToArray();
+
+                memoryStream.Close();
+                cryptoStream.Close();
+
+                cipherText = Convert.ToBase64String(cipherTextBytes);
+            }
+            catch
+            {
+                cipherText = string.Empty;
+            }
+
+            return cipherText;
+        }
+
+        /// <summary>
+        /// Decrypt data using Rijndael encryption.
+        /// </summary>
+        /// <param name=plainText></param>
+        /// <param name=passPhrase></param>
+        /// <param name=saltValue></param>
+        /// <param name=hashAlgorithm></param>
+        /// <param name=passwordIterations></param>
+        /// <param name=initVector></param>
+        /// <param name=keySize></param>
+        /// <returns></returns>
+        public static string Decrypt(string cipherText, string passPhrase, string saltValue, string hashAlgorithm, int passwordIterations, string initVector, int keySize)
+        {
+            string plainText = string.Empty;
+
+            try
+            {
+                byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+                byte[] saltValueBytes = Encoding.ASCII.GetBytes(saltValue);
+                byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+                PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, saltValueBytes, hashAlgorithm, passwordIterations);
+
+                byte[] keyBytes = password.GetBytes(keySize / 8);
+
+                RijndaelManaged symmetricKey = new RijndaelManaged();
+
+                symmetricKey.Mode = CipherMode.CBC;
+
+                ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+
+                MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+
+                byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+                int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+
+                memoryStream.Close();
+
+                cryptoStream.Close();
+
+                plainText = Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+            }
+            catch (CryptographicException)
+            {
+                throw;
+            }
+
+            return plainText;
+        }
+    }
+}
